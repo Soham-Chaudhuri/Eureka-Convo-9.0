f63f68b64295063f94adf4396f9ca80fbc02e446
Soham-Chaudhuri <94887725+Soham-Chaudhuri@users.noreply.github.com>
2024-03-23 11:32:46 +0530

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -7,14 +7,7 @@
 // // Your web app's Firebase configuration
 // // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // const firebaseConfig = {
-//   apiKey: "AIzaSyCqIpCzy9AXcuStcx0uZGZv9HSEEwv74Ho",
-//   authDomain: "eureka-convo-9.firebaseapp.com",
-//   projectId: "eureka-convo-9",
-//   storageBucket: "eureka-convo-9.appspot.com",
-//   messagingSenderId: "114608369941",
-//   appId: "1:114608369941:web:d090b48fd399331c9d791c",
-//   measurementId: "G-2XS16FJ9NP",
-//   databaseURL: "https://eureka-convo-9-default-rtdb.firebaseio.com",
+
 // };
 
 // // Initialize Firebase
@@ -29,14 +22,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBDwZqkhELEbMHWJP-nUQx1JnEQbdvOML0",
-  authDomain: "convo1-542d5.firebaseapp.com",
-  databaseURL: "https://convo1-542d5-default-rtdb.firebaseio.com",
-  projectId: "convo1-542d5",
-  storageBucket: "convo1-542d5.appspot.com",
-  messagingSenderId: "1069665287756",
-  appId: "1:1069665287756:web:2c2e7e49f6c72c335dbee9",
-  databaseURL: "https://convo1-542d5-default-rtdb.firebaseio.com",
+
 };
 
 // Initialize Firebase
